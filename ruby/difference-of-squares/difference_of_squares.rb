3a19e5d97f7555007d273d9f95b6d4f3c1699d5d
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-21 22:24:25 -0400

    solution for roman numerals problem

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -5,11 +5,11 @@ class Squares
   end
 
   def sum_of_squares
-    (1..@number).map { |x| x ** 2 }.inject :+
+    (1..@number).map { |x| x ** 2 }.reduce :+
   end
 
   def square_of_sums
-    ((1..@number).inject :+) ** 2
+    ((1..@number).reduce :+) ** 2
   end
 
   def difference
