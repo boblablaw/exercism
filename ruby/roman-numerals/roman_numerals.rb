9cfd58bdb12ab25e85dbcdc99ba2e34f0b595e54
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-21 22:23:47 -0400

    solution for roman numerals problem

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,15 @@
+class Fixnum
+  ROMAN_NUMERALS = {
+    1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD',
+     100 => 'C',  90 => 'XC',  50 => 'L',  40 => 'XL',
+      10 => 'X',   9 => 'IX',   5 => 'V',   4 => 'IV', 1 => 'I' }
+
+  def to_roman
+    number = self
+    ROMAN_NUMERALS.map do |divisor, roman|
+      quotient, modulus = number.divmod(divisor)
+      number = modulus
+      roman * quotient
+    end.join
+  end
+end
