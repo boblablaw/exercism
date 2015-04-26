1afdbeef687a6f350e380af99e1625d0d2163070
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-26 09:21:16 -0400

    add solution for prime sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -7,7 +7,7 @@ class Sieve
     numbers = [nil, nil, *2..@top]
 
     (2..Math.sqrt(@top)).each do |i|
-      (i**2..@top).step(i) { |m| numbers[m] = nil } #if numbers[i]
+      (i**2..@top).step(i) { |m| numbers[m] = nil } if numbers[i]
     end
     numbers.compact
   end
